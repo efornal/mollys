6e93955449cb8ccf2992b8e62cc298c63f6839d2
esteban <efornal@gmail.com>
2015-05-20 10:48:58 -0300

    functionality to display messages to the user

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from django.shortcuts import render
 from django.http import HttpResponse
 from app.models import Person, Office
+from django.template import Context
+from django.contrib import messages
 
 def index(request):
     return redirect('new')
