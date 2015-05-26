dcc39cb311f8b72b5099e506c278c27508f9e3c1
esteban <efornal@gmail.com>
2015-05-26 12:40:17 -0300

    changed office relationship

--- app/models.py
+++ app/models.py
@@ -1,28 +1,6 @@
 from django.db import models
 from datetime import datetime
 
-
-class Person(models.Model):
-    id = models.AutoField(primary_key=True,null=False)
-    name = models.CharField(max_length=200,null=False)
-    surname = models.CharField(max_length=200,null=False)
-    document_number = models.CharField(max_length=200,null=False)
-    document_type = models.IntegerField(null=False)
-    position = models.CharField(max_length=200,null=True)
-    office = models.CharField(max_length=200,null=True)
-    work_phone = models.CharField(max_length=200,null=True)
-    home_phone = models.CharField(max_length=200,null=True)
-    address = models.CharField(max_length=200,null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    
-    class Meta:
-        db_table = 'people'
-        verbose_name_plural = 'People'
-
-    def __unicode__(self):
-        return "%s" % (self.name)
-
     
 class Office(models.Model):
     id = models.AutoField(primary_key=True,null=False)
@@ -34,3 +12,25 @@ class Office(models.Model):
         
     def __unicode__(self):
         return "%s" % (self.name)
+
+class Person(models.Model):
+    id = models.AutoField(primary_key=True,null=False)
+    name = models.CharField(max_length=200,null=False)
+    surname = models.CharField(max_length=200,null=False)
+    document_number = models.CharField(max_length=200,null=False)
+    document_type = models.IntegerField(null=False)
+    position = models.CharField(max_length=200,null=True)
+    work_phone = models.CharField(max_length=200,null=True)
+    home_phone = models.CharField(max_length=200,null=True)
+    address = models.CharField(max_length=200,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    office = models.ForeignKey(Office, null=True, blank=True)
+    
+    class Meta:
+        db_table = 'people'
+        verbose_name_plural = 'People'
+
+    def __unicode__(self):
+        return "%s" % (self.name)
+
