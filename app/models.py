8d70cd54be970c3f75e2d082513832feedcb1766
esteban <efornal@gmail.com>
2015-06-12 12:09:28 -0300

    validation for ldap user in admin. setting to template setting

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from django.dispatch import receiver
 import logging
 import ldap
 from django.core.exceptions import ValidationError
+from django.conf import settings
 
 class DocumentType(models.Model):
     id = models.AutoField(primary_key=True,null=False)
@@ -31,12 +32,19 @@ class Office(models.Model):
         return "%s" % (self.name)
 
 class Person(models.Model):
+
+    def validate_existence_in_ldap(value):
+        if Person.exists_in_ldap(value):
+            raise ValidationError('El usuario %s ya existe.' % value)
+  
     document_regex = RegexValidator(regex=r'^\d{6,10}$',
                                     message="Ingrese un valor válido")
+    
     id = models.AutoField(primary_key=True,null=False)
     name = models.CharField(max_length=200,null=False)
     surname = models.CharField(max_length=200,null=False)
-    document_number = models.CharField(max_length=200,null=False,validators=[document_regex])
+    document_number = models.CharField(max_length=200,null=False,
+                                       validators=[document_regex])
     document_type = models.ForeignKey(DocumentType, null=False, blank=False)
     position = models.CharField(max_length=200,null=True, blank=True)
     work_phone = models.CharField(max_length=200,null=True)
@@ -46,7 +54,8 @@ class Person(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     office = models.ForeignKey(Office, null=True, blank=True)
     other_office = models.CharField(max_length=200,null=True)
-    ldap_user_name = models.CharField(max_length=200,null=True)
+    ldap_user_name = models.CharField(max_length=200,null=True,
+                                      validators=[validate_existence_in_ldap])
     received_application = models.BooleanField(default=False)
     
     class Meta:
@@ -64,14 +73,13 @@ class Person(models.Model):
         ldap_condition = "(uid=%s)" % uid
         logging.info("LDAP condition: %s" % ldap_condition)
         try:
-            l = ldap.initialize('ldap://ldap.intranet')
-            r = l.search_s('ou=People,dc=rectorado,dc=unl,dc=edu,dc=ar',
-                           ldap.SCOPE_SUBTREE, ldap_condition)
+            l = ldap.initialize( settings.LDAP_SERVER )
+            r = l.search_s(settings.LDAP_DN, ldap.SCOPE_SUBTREE, ldap_condition)
             
             for dn,entry in r:
-                logging.info("LDAP USER: %s=%s?" % (entry['uid'][0],uid))
+                logging.info("LDAP user search: %s=%s?" % (entry['uid'][0],uid))
                 if entry['uid'][0] == uid:
-                    logging.info("ENCONTRADO: %s" % entry['uid'][0])
+                    logging.info("LDAP User: %s, found!: " % entry['uid'][0])
                     return True
         except ldap.LDAPError, e:
             logging.error(e)
