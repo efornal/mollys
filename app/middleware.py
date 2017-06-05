902c7493eca1f496964bff38e1bca2cf6f211eab
esteban <efornal@gmail.com>
2017-06-05 09:31:30 -0300

    Added middleware to force language

---
+++ app/middleware.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.conf import settings
+from django.utils import translation
+
+class ForceLangMiddleware:
+
+    def process_request(self, request):
+        request.LANG = getattr(settings, 'LANGUAGE_CODE', settings.LANGUAGE_CODE)
+        translation.activate(request.LANG)
+        request.LANGUAGE_CODE = request.LANG
