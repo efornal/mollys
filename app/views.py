d862ad7b71880ee3cffe1fce4f9d60c425da06f2
Esteban <efornal@gmail.com>
2023-02-24 14:24:50 -0300

    removed option to print application

--- app/views.py
+++ app/views.py
@@ -120,8 +120,7 @@ def create(request):
 def outcome_success(request,form):
     from django.core.urlresolvers import reverse
     msg = _("outcome_success") % {'name': form.name,
-                                  'surname': form.surname,
-                                  'url': reverse('print_request', args=(form.id,)) }
+                                  'surname': form.surname}
 
     context = {'form': form, 'msg': msg}
     return render(request, 'outcome_success.html', context)
@@ -134,115 +133,115 @@ def outcome_error(request,form):
     return render(request, 'outcome_error.html', context)
 
 
-def parag_style():
-    from  reportlab.lib.styles import ParagraphStyle
-    from reportlab.lib.enums import TA_LEFT
-    style = ParagraphStyle('test')
-    style.textColor = 'black'
-    style.borderColor = 'black'
-    style.borderWidth = 0
-    style.alignment = TA_LEFT
-    style.fontSize = 9
-    return style
+# def parag_style():
+#     from  reportlab.lib.styles import ParagraphStyle
+#     from reportlab.lib.enums import TA_LEFT
+#     style = ParagraphStyle('test')
+#     style.textColor = 'black'
+#     style.borderColor = 'black'
+#     style.borderWidth = 0
+#     style.alignment = TA_LEFT
+#     style.fontSize = 9
+#     return style
 
 
-def write_header(canvas, x, y, ancho, alto):
-    from django.conf import settings
-    from reportlab.platypus import Paragraph
-    from reportlab.lib.units import cm
-    # header
-    fecha = datetime.now().strftime("%d/%m/%y %H:%M")
-    if settings.STATIC_ROOT:
-        img_path = "%s/images/logo_conduct_header.png" % settings.STATIC_ROOT
-    else:
-        img_path = "%s%simages/logo_conduct_header.png" % (settings.BASE_DIR,settings.STATIC_URL)
-    canvas.drawImage(img_path, x, y, 1.5*cm, 1.5*cm)
-    canvas.line(x,y-0.1*cm,ancho,y-0.1*cm)
-    parag = Paragraph( _("header_code"),parag_style())
-    parag.wrapOn(canvas,ancho-x,500)
-    parag.drawOn(canvas, x + 1.6*cm, y)
-    canvas.drawString(ancho-3*cm, y, fecha)
+# def write_header(canvas, x, y, ancho, alto):
+#     from django.conf import settings
+#     from reportlab.platypus import Paragraph
+#     from reportlab.lib.units import cm
+#     # header
+#     fecha = datetime.now().strftime("%d/%m/%y %H:%M")
+#     if settings.STATIC_ROOT:
+#         img_path = "%s/images/logo_conduct_header.png" % settings.STATIC_ROOT
+#     else:
+#         img_path = "%s%simages/logo_conduct_header.png" % (settings.BASE_DIR,settings.STATIC_URL)
+#     canvas.drawImage(img_path, x, y, 1.5*cm, 1.5*cm)
+#     canvas.line(x,y-0.1*cm,ancho,y-0.1*cm)
+#     parag = Paragraph( _("header_code"),parag_style())
+#     parag.wrapOn(canvas,ancho-x,500)
+#     parag.drawOn(canvas, x + 1.6*cm, y)
+#     canvas.drawString(ancho-3*cm, y, fecha)
 
 
-def print_request (request, person_id):
-    from reportlab.platypus import Paragraph
-    from reportlab.lib.pagesizes import A4
-    from reportlab.lib.units import cm,inch
-    from reportlab.platypus import Paragraph
-    from reportlab.lib.styles import ParagraphStyle
+# def print_request (request, person_id):
+#     from reportlab.platypus import Paragraph
+#     from reportlab.lib.pagesizes import A4
+#     from reportlab.lib.units import cm,inch
+#     from reportlab.platypus import Paragraph
+#     from reportlab.lib.styles import ParagraphStyle
 
-    person = Person.objects.get(id=person_id)
+#     person = Person.objects.get(id=person_id)
     
-    # configuration
-    x = 1.5*cm
-    y = 1.5*cm
-    dy = 0.5*inch
-    top = 29.7*cm - y - 1.2*y
-    right = 21*cm - x 
-    xtext = x + 3.7*cm
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="solicitud.pdf"'
+#     # configuration
+#     x = 1.5*cm
+#     y = 1.5*cm
+#     dy = 0.5*inch
+#     top = 29.7*cm - y - 1.2*y
+#     right = 21*cm - x 
+#     xtext = x + 3.7*cm
+#     response = HttpResponse(content_type='application/pdf')
+#     response['Content-Disposition'] = 'attachment; filename="solicitud.pdf"'
 
-    p = canvas.Canvas(response, pagesize=A4)
+#     p = canvas.Canvas(response, pagesize=A4)
 
-    write_header(p, x,top,right,200)
+#     write_header(p, x,top,right,200)
     
-    # title
-    p.drawString(xtext,top-inch,_("enabling_account").upper())
+#     # title
+#     p.drawString(xtext,top-inch,_("enabling_account").upper())
 
-    line = 4
-    p.drawString(x,top-line*dy, _("applicant") )
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    p.drawString(xtext,top-line*dy,person.surname_and_name())
-    line+=1
-    p.drawString(x,top-line*dy, _("doc_type") )
-    p.drawString(xtext+3*cm,top-line*dy, _("doc_number") )
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    p.drawString(xtext,top-line*dy, person.document_type.name )
-    p.drawString(xtext + 7*cm,top-line*dy, person.document_number )
-    line+=1
-    p.drawString(x,top-line*dy, _("position") )
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    p.drawString(xtext,top-line*dy,person.position)
-    line+=1
-    p.drawString(x,top-line*dy, _("work_phone"))
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    p.drawString(xtext,top-line*dy, person.work_phone or "")
-    line+=1
-    p.drawString(x,top-line*dy, _("home_phone"))
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    p.drawString(xtext,top-line*dy, person.home_phone or "")
-    line+=1
-    p.drawString(x,top-line*dy, _("address"))
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    p.drawString(xtext,top-line*dy, person.address )
-    line+=1
-    p.drawString(x,top-line*dy, _("short_office"))
-    p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
-    if person.office:
-        p.drawString(xtext,top-line*dy, person.office.name)
-    else:
-        p.drawString(xtext,top-line*dy, person.other_office)
+#     line = 4
+#     p.drawString(x,top-line*dy, _("applicant") )
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     p.drawString(xtext,top-line*dy,person.surname_and_name())
+#     line+=1
+#     p.drawString(x,top-line*dy, _("doc_type") )
+#     p.drawString(xtext+3*cm,top-line*dy, _("doc_number") )
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     p.drawString(xtext,top-line*dy, person.document_type.name )
+#     p.drawString(xtext + 7*cm,top-line*dy, person.document_number )
+#     line+=1
+#     p.drawString(x,top-line*dy, _("position") )
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     p.drawString(xtext,top-line*dy,person.position)
+#     line+=1
+#     p.drawString(x,top-line*dy, _("work_phone"))
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     p.drawString(xtext,top-line*dy, person.work_phone or "")
+#     line+=1
+#     p.drawString(x,top-line*dy, _("home_phone"))
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     p.drawString(xtext,top-line*dy, person.home_phone or "")
+#     line+=1
+#     p.drawString(x,top-line*dy, _("address"))
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     p.drawString(xtext,top-line*dy, person.address )
+#     line+=1
+#     p.drawString(x,top-line*dy, _("short_office"))
+#     p.line(xtext,top-line*dy-3,right-4*cm,top-line*dy-3)
+#     if person.office:
+#         p.drawString(xtext,top-line*dy, person.office.name)
+#     else:
+#         p.drawString(xtext,top-line*dy, person.other_office)
         
-    parag = Paragraph(_("intro_code"), parag_style())
-    parag.wrapOn(p,right-x,500)
-    parag.drawOn(p, x,y+0.5*inch)
+#     parag = Paragraph(_("intro_code"), parag_style())
+#     parag.wrapOn(p,right-x,500)
+#     parag.drawOn(p, x,y+0.5*inch)
 
-    p.showPage() # new page
+#     p.showPage() # new page
 
-    write_header(p, x,top,right,200)
+#     write_header(p, x,top,right,200)
         
-    parag = Paragraph( _("conduct_code"), parag_style())
-    parag.wrapOn(p,right-x,500)
-    parag.drawOn(p, x,top-550)
+#     parag = Paragraph( _("conduct_code"), parag_style())
+#     parag.wrapOn(p,right-x,500)
+#     parag.drawOn(p, x,top-550)
 
-    p.drawString(x,y+3*cm, _("date"))
+#     p.drawString(x,y+3*cm, _("date"))
 
-    p.drawString(x+3*cm,y, _("firm"))
-    p.line(x,y+0.5*cm,250,y+0.5*cm)
+#     p.drawString(x+3*cm,y, _("firm"))
+#     p.line(x,y+0.5*cm,250,y+0.5*cm)
 
-    p.drawString(x+8.4*cm,y,_('firm_responsible'))
-    p.line(280,y+0.5*cm,right,y+0.5*cm)
+#     p.drawString(x+8.4*cm,y,_('firm_responsible'))
+#     p.line(280,y+0.5*cm,right,y+0.5*cm)
 
-    p.save()
-    return response    
+#     p.save()
+#     return response    
