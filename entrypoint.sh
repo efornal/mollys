4496f4647d88021a8d4804033e43294e3d88a95f
Esteban <efornal@gmail.com>
2023-07-11 11:18:03 -0300

    generic conditioning

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -n "$DB_NAME" ] && [ ${#DB_NAME} -g 1 ]; then 
+if [ -z "$SKIP_APP_INIT" ]; then
     python manage.py compilemessages
     python manage.py collectstatic --noinput
     python manage.py migrate --database db_owner
