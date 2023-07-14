9ae2c636d58babbf6d13c3ca5ce9d71a0c7e7f9f
Esteban <efornal@gmail.com>
2023-07-14 08:41:13 -0300

    added informational message

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 echo "Status SKIP_APP_INIT: $SKIP_APP_INIT"
 
 if [ -z "$SKIP_APP_INIT" ]; then
+    echo "Running application initialization scripts.."
     python manage.py compilemessages
     python manage.py collectstatic --noinput
     python manage.py migrate --database db_owner
